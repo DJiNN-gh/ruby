f119232dc16e688c74950a0d50ebc3423836e878
DJiNN-gt <allaneumermo@gmail.com>
2023-05-12 16:56:12 -0300

    Update Friday 12 May 16:56 OpenSUSE Leap

---
+++ laco.rb
@@ -0,0 +1,16 @@
+# Este programa propõe-se a demonstrar a execução de um laço (loop)
+
+i = 0
+# Iniciando do zero
+
+loop do
+    
+    puts i
+    i = i +1
+
+    if i>=10
+    # Contando de 0 a 9, dez representações
+        
+        break    
+    end
+end
