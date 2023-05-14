80af705bf35ba58b5ad684e839599f081e43f3f9
DJiNN-gh <allaneumermo@gmail.com>
2023-05-13 21:40:22 -0300

    Update Saturday 13 May 21:40 Endeavour OS

--- laco.rb
+++ laco.rb
@@ -4,8 +4,10 @@ i = 0
 # Iniciando do zero
 
 loop do
-    
+# Início do loop, método loop do    
     puts i
+    # Uso do método puts, que automaticamente quebra uma linha a cada impressão
+    # Alternativamente pode-se utilizar o método print que não quebra linhas automaticamente
     i = i +1
 
     if i>=10
@@ -14,3 +16,18 @@ loop do
         break    
     end
 end
+3.times { print '-' }
+
+puts
+
+1.upto(10) { puts i = i - 1 }
+# Agora utilizando o método upto chamado uma vez e imprimindo a partir do 9 até o 0
+
+3.times { print '-' }
+
+puts
+
+10.times { puts i = i + 1}
+# Agora utilizando o método times chamado dez vezes e imprimindo a partir do 1 até o 10
+
+puts
